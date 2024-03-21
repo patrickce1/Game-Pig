8ac167bbd635bb66109fe1e161cabf090220781a
Dhilan Shaan Bansal <dsb288@en-ci-cisugcl16..coecis.cornell.edu>
2024-03-21 02:28:57 -0400

    Working Prototype

--- backend/helpers/__init__.py
+++ backend/helpers/__init__.py
@@ -2,8 +2,6 @@ import ast
 import json
 import re
 import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 def tokenize(text):
     # Starting with a simple tokenization pattern
     # This pattern does not cover all Treebank tokenizer rules but gives a basic idea
@@ -39,44 +37,16 @@ def jaccard_similarity(tokens1, tokens2):
     return len(set1 & set2) / len(set1 | set2) if len(set1 | set2) > 0 else 0
 
 def compute_similarity_score(game_reviews_dict, query):
-    vectorizer = TfidfVectorizer()
-
-    cosine_scores = {}
+    query_tokens = tokenize(query.lower())
+    similarity_scores = {}
 
     for game, reviews in game_reviews_dict.items():
-        if reviews:
-            # Combine the reviews with the query for TF-IDF transformation
-            all_texts = reviews + [query]
+        review_tokens = [token for review in reviews for token in review]
+        similarity = jaccard_similarity(review_tokens, query_tokens)
+        similarity_scores[game] = similarity
+        sorted_similarity_scores = sorted(similarity_scores.items(), key=lambda x: x[1], reverse=True)
 
-            # Transform texts to a TF-IDF matrix
-            tfidf_matrix = vectorizer.fit_transform(all_texts)
-
-            # Compute cosine similarity between the query and each review
-            # The query is the last document in the matrix
-            cos_similarities = cosine_similarity(tfidf_matrix[-1:], tfidf_matrix[:-1])
-
-            # Calculate the average cosine similarity score for the game
-            average_score = np.mean(cos_similarities)
-
-            cosine_scores[game] = average_score
-
-    # Sort the games by their average cosine similarity score in descending order
-    sorted_cosine_scores = sorted(cosine_scores.items(), key=lambda x: x[1], reverse=True)
-
-    return sorted_cosine_scores
-
-
-
-    # query_tokens = tokenize(query.lower())
-    # similarity_scores = {}
-
-    # for game, reviews in game_reviews_dict.items():
-    #     review_tokens = [token for review in reviews for token in review]
-    #     similarity = jaccard_similarity(review_tokens, query_tokens)
-    #     similarity_scores[game] = similarity
-    #     sorted_similarity_scores = sorted(similarity_scores.items(), key=lambda x: x[1], reverse=True)
-
-    # return sorted_similarity_scores
+    return sorted_similarity_scores
 
 
 
