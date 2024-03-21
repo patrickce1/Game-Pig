e7ae797ae35443cf975771b3623e790f1fbcd11e
Dhilan Shaan Bansal <dsb288@en-ci-cisugcl20..coecis.cornell.edu>
2024-03-21 00:34:18 -0400

    test

--- backend/app.py
+++ backend/app.py
@@ -18,55 +18,39 @@ json_file_path = os.path.join(current_directory, 'init.json')
 
 # Assuming your JSON data is stored in a file named 'init.json'
 with open(json_file_path, 'r') as file:
-     data = json.load(file)
-#     episodes_df = pd.DataFrame(data['episodes'])
-#     reviews_df = pd.DataFrame(data['reviews'])
-tokenizer = TreebankWordTokenizer()
-game_reviews_dict = {}
-for item in data:
-    game_name = item['Game']
-    reviews_str = item['Review']
-    reviews = ast.literal_eval(reviews_str)
-    tokenized_reviews = [tokenizer.tokenize(review) for review in reviews]
+    data = json.load(file)
+    episodes_df = pd.DataFrame(data['episodes'])
+    reviews_df = pd.DataFrame(data['reviews'])
+# tokenizer = TreebankWordTokenizer()
+# game_reviews_dict = {}
+# for item in data:
+#     game_name = item['Game']
+#     reviews_str = item['Review']
+#     reviews = ast.literal_eval(reviews_str)
+#     tokenized_reviews = [tokenizer.tokenize(review) for review in reviews]
     
-    game_reviews_dict[game_name] = tokenized_reviews
-
-# Check a few entries from the dictionary
-# for game, reviews in list(game_reviews_dict.items())[:1]:
-#     print(game, reviews[:2])
+#     game_reviews_dict[game_name] = tokenized_reviews
 
 
 
-def jaccard_similarity(tokens1, tokens2):
-    set1 = set(tokens1)
-    set2 = set(tokens2)
-    return len(set1 & set2) / len(set1 | set2) if len(set1 | set2) > 0 else 0
+# def jaccard_similarity(tokens1, tokens2):
+#     set1 = set(tokens1)
+#     set2 = set(tokens2)
+#     return len(set1 & set2) / len(set1 | set2) if len(set1 | set2) > 0 else 0
 
-# Function to compute Jaccard similarity between game reviews and a query string
-def compute_similarity_with_query(game_reviews_dict, query):
-    query_tokens = tokenizer.tokenize(query.lower())
-    similarity_scores = {}
+# def compute_similarity_with_query(game_reviews_dict, query):
+#     query_tokens = tokenizer.tokenize(query.lower())
+#     similarity_scores = {}
 
-    for game, reviews in game_reviews_dict.items():
-        # Flatten the list of tokenized reviews into a single list of tokens
-        review_tokens = [token for review in reviews for token in review]
-        
-        # Compute Jaccard similarity
-        similarity = jaccard_similarity(review_tokens, query_tokens)
-        similarity_scores[game] = similarity
-        sorted_similarity_scores = sorted(similarity_scores.items(), key=lambda x: x[1], reverse=True)
+#     for game, reviews in game_reviews_dict.items():
+#         review_tokens = [token for review in reviews for token in review]
+#         similarity = jaccard_similarity(review_tokens, query_tokens)
+#         similarity_scores[game] = similarity
+#         sorted_similarity_scores = sorted(similarity_scores.items(), key=lambda x: x[1], reverse=True)
 
-    return sorted_similarity_scores
+#     return sorted_similarity_scores
 
-# # Example query string
-# query = "action adventure"
 
-# # Compute the similarity scores
-# similarity_scores = compute_similarity_with_query(game_reviews_dict, query)
-
-# # Display the similarity scores
-# for game, score in similarity_scores[:5]:
-#     print(f"{game}: {score}")
 
 
 
@@ -76,17 +60,17 @@ CORS(app)
 
 # Sample search using json with pandas
 def json_search(query):
-    # matches = []
-    # merged_df = pd.merge(episodes_df, reviews_df, left_on='id', right_on='id', how='inner')
-    # matches = merged_df[merged_df['title'].str.lower().str.contains(query.lower())]
-    # matches_filtered = matches[['title', 'descr', 'imdb_rating']]
-    # matches_filtered_json = matches_filtered.to_json(orient='records')
-    # return matches_filtered_json
-    similarity_scores = compute_similarity_with_query(game_reviews_dict, query)
-    scores_df = pd.DataFrame(list(similarity_scores.items()), columns=['Game', 'Score'])
-    top_matches = scores_df.nlargest(3, 'Score')
-    top_matches_json = top_matches_filtered.to_json(orient='records')
-    return top_matches_json
+    matches = []
+    merged_df = pd.merge(episodes_df, reviews_df, left_on='id', right_on='id', how='inner')
+    matches = merged_df[merged_df['title'].str.lower().str.contains(query.lower())]
+    matches_filtered = matches[['title', 'descr', 'imdb_rating']]
+    matches_filtered_json = matches_filtered.to_json(orient='records')
+    return matches_filtered_json
+    # similarity_scores = compute_similarity_with_query(game_reviews_dict, query)
+    # scores_df = pd.DataFrame(list(similarity_scores.items()), columns=['Game', 'Score'])
+    # top_matches = scores_df.nlargest(3, 'Score')
+    # top_matches_json = top_matches_filtered.to_json(orient='records')
+    # return top_matches_json
 @app.route("/")
 def home():
     return render_template('base.html',title="sample html")
