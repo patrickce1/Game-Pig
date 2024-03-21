9e15fe29a91d05a57722518c9cdb042fec1bea65
Dhilan Shaan Bansal <dsb288@en-ci-cisugcl20..coecis.cornell.edu>
2024-03-21 10:51:55 -0400

    small change

--- backend/helpers/__init__.py
+++ backend/helpers/__init__.py
@@ -3,17 +3,13 @@ import json
 import re
 import numpy as np
 def tokenize(text):
-    # Starting with a simple tokenization pattern
-    # This pattern does not cover all Treebank tokenizer rules but gives a basic idea
-    token_pattern = r'''(?x)          # Set flag to allow verbose regexps
-        (?:[A-Z]\.)+                  # Abbreviations, e.g., U.S.A.
-        | \w+(?:-\w+)*                # Words with optional internal hyphens
-        | \$?\d+(?:\.\d+)?%?          # Currency and percentages, e.g., $12.40, 82%
-        | \.\.\.                      # Ellipsis
-        | [][.,;"'?():_`-]            # These are separate tokens; includes ], [
+    token_pattern = r'''(?x)          
+        (?:[A-Z]\.)+                 
+        | \w+(?:-\w+)*                
+        | \$?\d+(?:\.\d+)?%?          
+        | \.\.\.                      
+        | [][.,;"'?():_`-]            
     '''
-
-    # Using regular expression to split the text into tokens
     tokens = re.findall(token_pattern, text)
 
     return tokens
@@ -55,20 +51,20 @@ def compute_similarity_score(game_reviews_dict, query):
     # similarity_scores = {}
 
     # for game, reviews in game_reviews_dict.items():
-    #     # Compute similarity for each review and collect the scores
+
     #     review_scores = []
     #     for review in reviews:
     #         review_tokens = set(tokenize(review.lower()))
     #         score = jaccard_similarity(review_tokens, query_tokens)
     #         review_scores.append(score)
 
-    #     # Calculate the average similarity score for the game
+
     #     average_score = np.max(review_scores)
 
 
     #     similarity_scores[game] = average_score
 
-    # # Sort the games by their average similarity score in descending order
+
     # sorted_similarity_scores = sorted(similarity_scores.items(), key=lambda x: x[1], reverse=True)
 
     # return sorted_similarity_scores
